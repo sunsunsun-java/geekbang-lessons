0668d77a1a389d5f7ffd7a380847e0689d8c9280
fromzerobegin <fromzerobegin@gmail.com>
2023-11-14 00:52:32 +0800

    generic add

---
+++ thinking-in-spring/generic/src/main/java/org/geekbang/thinking/in/spring/generic/GenericTypeResolverDemo.java
@@ -0,0 +1,53 @@
+package org.geekbang.thinking.in.spring.generic;
+
+import org.springframework.core.GenericTypeResolver;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.core.GenericTypeResolver.resolveReturnType;
+import static org.springframework.core.GenericTypeResolver.resolveReturnTypeArgument;
+
+/**
+ * {@link GenericTypeResolver} 示例
+ */
+public class GenericTypeResolverDemo {
+
+    public static void main(String[] args) throws NoSuchMethodException {
+        // String 是 Comparable<String> 具体化
+        displayReturnTypeGenericInfo(GenericTypeResolverDemo.class, Comparable.class, "getString");
+
+        // ArrayList<Object> 是 List 泛型参数类型的具体化
+        displayReturnTypeGenericInfo(GenericTypeResolverDemo.class, List.class, "getList");
+
+        // StringList 也是 List 泛型参数类型的具体化
+        displayReturnTypeGenericInfo(GenericTypeResolverDemo.class, List.class, "getStringList");
+    }
+
+    public static StringList getStringList() {
+        return null;
+    }
+
+    public static ArrayList<Object> getList() { // 泛型参数类型具体化
+        return null;
+    }
+
+    public static String getString() {
+        return null;
+    }
+
+    private static void displayReturnTypeGenericInfo(Class<?> containingClass, Class<?> genericIfc, String methodName) throws NoSuchMethodException {
+        Method method = containingClass.getMethod(methodName);
+
+        // 声明类 GenericTypeResolverDemo.class
+        Class<?> returnType = resolveReturnType(method, containingClass);
+
+        // 常规类作为方法返回值
+        System.out.printf("GenericTypeResolver.resolveReturnType(%s,%s) = %s\n", methodName, containingClass.getSimpleName(), returnType);
+        // 常规类型不具备泛型参数类型 List<E>
+        Class<?> returnTypeArgument = resolveReturnTypeArgument(method, genericIfc);
+        System.out.printf("GenericTypeResolver.resolveReturnTypeArgument(%s,%s) = %s\n", methodName, containingClass.getSimpleName(), returnTypeArgument);
+
+    }
+}
---
+++ thinking-in-spring/generic/src/main/java/org/geekbang/thinking/in/spring/generic/StringList.java
@@ -0,0 +1,6 @@
+package org.geekbang.thinking.in.spring.generic;
+
+import java.util.ArrayList;
+
+public class StringList extends ArrayList<String> { // 泛型参数具体化（字节码有记录）
+}
