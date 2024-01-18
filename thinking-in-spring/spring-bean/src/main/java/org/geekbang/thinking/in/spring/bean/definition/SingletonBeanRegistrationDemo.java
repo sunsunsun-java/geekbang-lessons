b7bffff6fde644f94af15c4f4d14e1091f836b54
fromzerobegin <fromzerobegin@gmail.com>
2024-01-18 19:28:27 +0800

    The ssh connection test

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/SingletonBeanRegistrationDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/SingletonBeanRegistrationDemo.java
@@ -3,6 +3,7 @@ package org.geekbang.thinking.in.spring.bean.definition;
 import org.geekbang.thinking.in.spring.bean.factory.DefaultUserFactory;
 import org.geekbang.thinking.in.spring.bean.factory.UserFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.SingletonBeanRegistry;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -15,14 +16,14 @@ public class SingletonBeanRegistrationDemo {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         // 创建外部 userFactory 对象
         UserFactory userFactory = new DefaultUserFactory();
-        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+        SingletonBeanRegistry singletonBeanRegistry = applicationContext.getBeanFactory();
         // 创建外部单例对象
-        beanFactory.registerSingleton("userFactory", userFactory);
+        singletonBeanRegistry.registerSingleton("userFactory", userFactory);
         // 启动 Spring 上下文
         applicationContext.refresh();
 
         // 通过依赖查找的方式来获取 UserFactory
-        UserFactory userFactoryByLookup = beanFactory.getBean("userFactory", UserFactory.class);
+        UserFactory userFactoryByLookup = applicationContext.getBean("userFactory", UserFactory.class);
         System.out.println("userFactory == userFactoryByLookup : " + (userFactory == userFactoryByLookup));
 
         // 关闭 Spring 应用上下文
